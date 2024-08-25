1af487ec6b2a9878ec16e391a67746760a05dfc2
Hector Ramos <121777320+seb1124@users.noreply.github.com>
2024-08-25 16:22:51 -0400

    Create SearchContacts.php

---
+++ LAMPAPI/SearchContacts.php
@@ -0,0 +1,59 @@
+<?php
+
+    //get JSON
+    $inData = getRequestInfo();
+
+    //init search results as empty string
+    $searchResults = "";
+    $searchCount = 0;
+
+    $conn = new mysqli("localhost", "Team14", "COP4331-Team14X", "COP4331");
+    if ($conn->connect_error) {
+        returnWithError($conn->connect_error);
+    }
+    else{
+        $stmnt = $conn->prepare("select Name from Contacts where Name like ? and UserID=?");
+        $contactName = "%" . $inData["search"] . "%";
+        $stmnt->bind_param("ss", $contactName, $inData["userID"]);
+        $stmnt->execute();
+
+        $result = $stmnt->get_result();
+
+        while($row = $result->fetch_assoc()) {
+            if($searchCount > 0){
+                $searchResults .= ",";
+            }
+            $searchCount++;
+            $searchResults .= '"' . $row["Name"] . '"';
+        }
+
+        if($searchCount == 0){
+            returnWithError("No Records Found");
+        }
+        else{
+            returnWithInfo($searchResults);
+        }
+
+        $stmnt->close();
+        $conn->close();
+    }
+
+    //------- Functions ---------
+    function getRequestInfo(){
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson($obj){
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError($err){
+        $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+        sendResultInfoAsJson($retValue);
+    }
+
+    function returnWithInfo($searchResults){
+        $retValue = '{"results":[' . $searchResults . '],"error":""}';
+        sendResultInfoAsJson($retValue);
+    }
