bd51602e91e8d33cfff0d7d58101acf13eedf91a
Hector Ramos <121777320+seb1124@users.noreply.github.com>
2024-09-12 10:47:43 -0400

    Update SearchContacts.php
    Changed search results to output ALL contact info not just names

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -3,15 +3,17 @@
     //get JSON
     $inData = getRequestInfo();
 
-    //init search results as empty string
+    //init search results as empty string & count as 0
     $searchResults = "";
     $searchCount = 0;
 
+    //connect to database
     $conn = new mysqli("localhost", "Team14", "COP4331-Team14X", "COP4331");
     if ($conn->connect_error) {
         returnWithError($conn->connect_error);
     }
     else{
+        //prepare SQL statement to search for contacts matching inputted name and userID
         $stmnt = $conn->prepare("select * from Contacts where Name like ? and UserID=?");
         $contactName = "%" . $inData["search"] . "%";
         $stmnt->bind_param("ss", $contactName, $inData["userID"]);
@@ -19,19 +21,21 @@
 
         $result = $stmnt->get_result();
 
+        //iterate through results and append them to searchResults string
         while($row = $result->fetch_assoc()) {
             if($searchCount > 0){
-                $searchResults .= "," . PHP_EOL;
+                $searchResults .= ",";
             }
-            //increment search count and append search results
             $searchCount++;
             $searchResults .= '{"Name":"' . $row["Name"]. '", "Email":"' .$row["Email"]. '", "Phone":"' .$row["Phone"]. '", "ID":"' .$row["ID"]. '", "UserID":"' .$row["UserID"]. '"}';
 
         }
 
+        //if no records found
         if($searchCount == 0){
             returnWithError("No Records Found");
         }
+        //if records found
         else{
             returnWithInfo($searchResults);
         }
